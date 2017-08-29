08f5c53b7d234a16d985fe7f4af1b49ac41ccb85
Heaton <n0231712@libp45p-31207dd.lm.lmig.com>
2017-08-29 09:01:28 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,8 +22,10 @@ cardImage: "images/king-of-diamonds.png"
 ];
 var cardsInPlay = [];
 
-//function for checking if the two cards match
-var checkForMatch = function (){
+
+//function for showing the front of the clicked card and checking if the two cards match
+var checkForMatch = function (cardId){
+	this['src'] = cards[cardId].cardImage;
 	if (cardsInPlay.length===2){
 		if (cardsInPlay[0]===cardsInPlay[1]) {
 			alert("You found a match!");
@@ -33,27 +35,28 @@ var checkForMatch = function (){
 		}
 	}
 };
-//function for adding the flipped card to the array
-var flipCard = function (cardId){
+
+//function for getting the ID of the clicked card and adding the flipped card to the array
+var flipCard = function (){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
 
-	checkForMatch();
+	checkForMatch(cardId);
 };
 
-flipCard(0);
-flipCard(2);
+//function for creating board with cards and settting up click event listener
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
 
-
-
-
-
-
-
-
-
-
-
+createBoard();
 
